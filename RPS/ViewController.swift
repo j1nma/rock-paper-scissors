28c83e92532e68abe0b62b148efa2e08c65e003c
j1nma <j1nma@users.noreply.github.com>
2020-07-14 07:37:08 +0200

    finished logic and game

--- RPS/ViewController.swift
+++ RPS/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
 
     var gameStatus: GameState = .start
 
-    @IBOutlet weak var appSign: UILabel!
+    @IBOutlet weak var appSignLabel: UILabel!
     @IBOutlet weak var gameStatusLabel: UILabel!
     
     @IBOutlet weak var playerRockButton: UIButton!
@@ -26,22 +26,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var playAgainButton: UIButton!
     
     @IBAction func playedRock(_ sender: Any) {
-        
+        play(.rock)
         updateUI()
     }
     
     @IBAction func playedPaper(_ sender: Any) {
-        
+        play(.paper)
         updateUI()
     }
     
     @IBAction func playedScissors(_ sender: Any) {
-        
+        play(.scissors)
         updateUI()
     }
     
     @IBAction func playAgain(_ sender: Any) {
-        updateUI()
+        gameStatus = .start
+        viewDidLoad()
     }
     
     func updateUI() {
@@ -49,11 +50,13 @@ class ViewController: UIViewController {
         self.view.backgroundColor = getColor(gameStatus)
         
         if gameStatus == .start {
-            appSign.text = "🤖"
+            appSignLabel.text = "🤖"
             playAgainButton.isHidden = true
+            
             playerRockButton.isEnabled = true
             playerPaperButton.isEnabled = true
             playerScissorsButton.isEnabled = true
+            
             playerRockButton.isHidden = false
             playerPaperButton.isHidden = false
             playerScissorsButton.isHidden = false
@@ -71,6 +74,32 @@ class ViewController: UIViewController {
         case .draw:
             return .blue
         }
+    }
+    
+    func play(_ playerSign: Sign) {
+        let appSign = randomSign()
+        appSignLabel.text = appSign.emoji
+        
+        playerRockButton.isEnabled = false
+        playerPaperButton.isEnabled = false
+        playerScissorsButton.isEnabled = false
+        
+        playerRockButton.isHidden = true
+        playerPaperButton.isHidden = true
+        playerScissorsButton.isHidden = true
+        
+        switch playerSign {
+            case .rock:
+                playerRockButton.isHidden = false
+            case .paper:
+                playerPaperButton.isHidden = false
+            case .scissors:
+                playerScissorsButton.isHidden = false
+        }
+        
+        gameStatus = playerSign.compare(other: appSign)
+        
+        playAgainButton.isHidden = false
     }
 }
 
